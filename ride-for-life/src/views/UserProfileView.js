2041d65c97165058f9655fed4792b0f110f3006b
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-03-27 13:29:30 -0400

    Riders from api rendered on screen

--- ride-for-life/src/views/UserProfileView.js
+++ ride-for-life/src/views/UserProfileView.js
@@ -1,23 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { userData } from '../actions';
+import UserProfile from '../components/UserProfile';
 
 class UserProfileView extends React.Component {
     componentDidMount() {
       console.log('CDM')
-      this.props.getData();
+      this.props.userData();
     }
     
 
     render() {
+        console.log('User Rendering...')
         return (
-            <div>
+            <div className="rider-list">
                 <h1>Riders</h1>
                 {this.props.users.map((user, id) => (
                     <ul key={id}>
-                        <h1>{user.firstname}</h1>
-                    </ul>
+                        <p>Name: {user.firstname}</p>
+                        <p>Phone: {user.phone}</p>
+                        <p>Location: {user.location}</p>
+                    
+                <Link to={`/users/${user.user_id}`}><UserProfile user={user}/>
+                </Link></ul>
                 ))}
+
             </div>
         )
     }
