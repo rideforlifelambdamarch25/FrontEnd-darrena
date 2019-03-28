03f2b4254dceb3ca72a76aef8eebc341089b99b4
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-03-28 14:53:13 -0400

    Working on rendering individual driver profiles on click.  Currently getting ×

--- ride-for-life/src/components/DriverList.js
+++ ride-for-life/src/components/DriverList.js
@@ -3,9 +3,9 @@ import React from 'react';
 const DriverList = props => {
     const { firstname, lastname, phone, vehicle_type, location, price } = props.driver
     return(
-        <div>
-            <ul>
-                <p>Name: {firstname} {lastname}</p>
+        <div className="driver-card">
+            <ul onClick={() => routeToDriver(props, props.driver)}>
+                <p>{firstname} {lastname}</p>
                 <p>Phone Number: {phone}</p>
                 <p>Vehicle: {vehicle_type}</p>
                 <p>location: {location}</p>
@@ -15,6 +15,8 @@ const DriverList = props => {
     )
 }
 
-
+const routeToDriver = (props, driver) => {
+    props.history.push(`/drivers/${driver.driver_id}`);
+}
 
 export default DriverList;
